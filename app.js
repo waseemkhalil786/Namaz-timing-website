8d54adfcb325b10ad7326cb993afc3d191ca5486
Waseem khalil <waseemkhalilworl@gmail.com>
2024-05-31 22:24:58 -0700

    50% work sucsess

---
+++ app.js
@@ -0,0 +1,26 @@
+// document.querySelector(".btn").addEventListener("submit", function (event) {
+//     event.preventDefault();
+//     document.querySelector("#answer").innerHTML= "";
+//     const inputcity= document.querySelector("#input-city").value;
+//     const Nowcity = [
+//         "karachi",
+//         "lahore",
+//         "islamabad",
+//         "london",
+//         "paris",
+//         "berlin",
+//         "delhi",
+//         "moscow",
+//         "newyork",
+//         "dubai",
+//         "mumbai",
+//         "shanghai",
+//         "riyadh",
+//         "seoul",
+//         "tokyo",
+//         "dhaka",
+//         "tehran",
+//         "istanbul",    
+//     ];
+//     );
+    
